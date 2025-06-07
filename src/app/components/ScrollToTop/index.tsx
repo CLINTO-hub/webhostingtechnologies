5e12f1f6a6e11a4a6aed12a820f6054b7cee891c
Clinto George <clintogeorge007@gmail.com>
2025-06-07 17:19:45 +0530

    chat-button

--- src/app/components/ScrollToTop/index.tsx
+++ src/app/components/ScrollToTop/index.tsx
@@ -22,27 +22,37 @@ export default function ScrollToTop() {
     }
 
     window.addEventListener('scroll', toggleVisibility)
-
     return () => window.removeEventListener('scroll', toggleVisibility)
   }, [])
 
   return (
-    <div className='fixed bottom-8 right-8 z-999'>
-      <div className='flex items-center gap-2.5'>
-
+    <div className='fixed bottom-8 right-8 z-50'>
+      <div className='flex flex-row items-center gap-2.5'>
+        {/* Chat Now button for desktop: always visible */}
         <Link
           href='https://wa.me/919562857669'
           target='_blank'
-          className='lg:block bg-green-600 text-white hover:bg-darkmode text-sm px-4 py-3.5 leading-none rounded-lg font-medium text-nowrap'>
+          className='hidden md:inline-block bg-green-600 text-white hover:bg-green-700 text-sm px-4 py-3.5 leading-none rounded-lg font-medium text-nowrap'>
           Chat Now
         </Link>
+
+        {/* Chat Now and scroll to top for mobile (only when visible) */}
         {isVisible && (
-          <div
-            onClick={scrollToTop}
-            aria-label='scroll to top'
-            className='back-to-top flex h-10 w-10 cursor-pointer items-center justify-center rounded-md bg-[#102C46] text-white shadow-md transition duration-300 ease-in-out hover:bg-dark'>
-            <span className='mt-[6px] h-3 w-3 rotate-45 border-l border-t border-white'></span>
-          </div>
+          <>
+            <Link
+              href='https://wa.me/919562857669'
+              target='_blank'
+              className='inline-block md:hidden bg-green-600 text-white hover:bg-green-700 text-sm px-4 py-3.5 leading-none rounded-lg font-medium text-nowrap'>
+              Chat Now
+            </Link>
+
+            <div
+              onClick={scrollToTop}
+              aria-label='scroll to top'
+              className='back-to-top flex h-10 w-10 cursor-pointer items-center justify-center rounded-md bg-[#102C46] text-white shadow-md transition duration-300 ease-in-out hover:bg-dark'>
+              <span className='mt-[6px] h-3 w-3 rotate-45 border-l border-t border-white'></span>
+            </div>
+          </>
         )}
       </div>
     </div>
